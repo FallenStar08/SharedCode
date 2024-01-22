080974ae473c36fdad0cb97da709f06ffa3b9a8f
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-01-22 14:31:27 +0100

    Check for server context

--- Shared/Utils/Template.lua
+++ Shared/Utils/Template.lua
@@ -5,12 +5,14 @@ Template = {}
 -- -------------------------------------------------------------------------- --
 --                                  Templates                                 --
 -- -------------------------------------------------------------------------- --
-Template.GetAllCacheTemplates = Ext.Template.GetAllCacheTemplates
-Template.GetAllLocalCacheTemplates = Ext.Template.GetAllLocalCacheTemplates
-Template.GetAllLocalTemplates = Ext.Template.GetAllLocalTemplates
-Template.GetAllRootTemplates = Ext.Template.GetAllRootTemplates
-Template.GetCacheTemplate = Ext.Template.GetCacheTemplate
-Template.GetLocalCacheTemplate = Ext.Template.GetLocalCacheTemplate
-Template.GetLocalTemplate = Ext.Template.GetLocalTemplate
-Template.GetRootTemplate = Ext.Template.GetRootTemplate
-Template.GetTemplate = Ext.Template.GetTemplate
+if Ext.IsServer() then
+    Template.GetAllCacheTemplates = Ext.Template.GetAllCacheTemplates
+    Template.GetAllLocalCacheTemplates = Ext.Template.GetAllLocalCacheTemplates
+    Template.GetAllLocalTemplates = Ext.Template.GetAllLocalTemplates
+    Template.GetAllRootTemplates = Ext.Template.GetAllRootTemplates
+    Template.GetCacheTemplate = Ext.Template.GetCacheTemplate
+    Template.GetLocalCacheTemplate = Ext.Template.GetLocalCacheTemplate
+    Template.GetLocalTemplate = Ext.Template.GetLocalTemplate
+    Template.GetRootTemplate = Ext.Template.GetRootTemplate
+    Template.GetTemplate = Ext.Template.GetTemplate
+end
