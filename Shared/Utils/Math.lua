e9ad1f2e16a8a325b5ff466b9b7937c49ede8326
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-01-25 20:33:44 +0100

    Move Levenshtein & co to Math

---
+++ Shared/Utils/Math.lua
@@ -0,0 +1,87 @@
+function Levenshtein(str1, str2)
+    local len1, len2 = #str1 + 1, #str2 + 1
+    local matrix = {}
+    for i = 1, len1 do
+        matrix[i] = {}
+        for j = 1, len2 do
+            if i == 1 then
+                matrix[i][j] = j - 1
+            elseif j == 1 then
+                matrix[i][j] = i - 1
+            else
+                local cost = (str1:sub(i - 1, i - 1) ~= str2:sub(j - 1, j - 1) and 1 or 0)
+                matrix[i][j] = math.min(
+                    matrix[i - 1][j] + 1,
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j - 1] + cost
+                )
+            end
+        end
+    end
+    return matrix[len1][len2]
+end
+
+function JaroWinkler(str1, str2)
+    local len1, len2 = #str1, #str2
+    local maxLen = math.max(len1, len2)
+
+    if maxLen == 0 then
+        return 0
+    end
+
+    local matchWindow = math.floor(maxLen / 2) - 1
+
+    local matches = 0
+    local transpositions = 0
+
+    local str1Flags = {}
+    local str2Flags = {}
+
+    for i = 1, len1 do
+        local startMatch = math.max(1, i - matchWindow)
+        local endMatch = math.min(i + matchWindow, len2)
+
+        for j = startMatch, endMatch do
+            if not str2Flags[j] and str1:sub(i, i) == str2:sub(j, j) then
+                str1Flags[i] = true
+                str2Flags[j] = true
+                matches = matches + 1
+                break
+            end
+        end
+    end
+
+    if matches == 0 then
+        return 0
+    end
+
+    local k = 1
+    for i = 1, len1 do
+        if str1Flags[i] then
+            while not str2Flags[k] do
+                k = k + 1
+            end
+
+            if str1:sub(i, i) ~= str2:sub(k, k) then
+                transpositions = transpositions + 1
+            end
+
+            k = k + 1
+        end
+    end
+
+    local prefix = 0
+    for i = 1, math.min(4, math.min(len1, len2)) do
+        if str1:sub(i, i) == str2:sub(i, i) then
+            prefix = prefix + 1
+        else
+            break
+        end
+    end
+
+    local jaro = (matches / len1 + matches / len2 + (matches - transpositions / 2) / matches) / 3
+    local scalingFactor = 0.1
+    local jaroWinkler = jaro + prefix * scalingFactor * (1 - jaro)
+
+    return jaroWinkler
+end
