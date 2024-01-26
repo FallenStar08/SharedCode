68c26192affa2903802fc7c0833e5bb95f50bf11
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-01-26 17:44:29 +0100

    DeepIterate rewrite

--- Shared/Utils/Inventory.lua
+++ Shared/Utils/Inventory.lua
@@ -1,8 +1,8 @@
 ---@diagnostic disable: duplicate-set-field
 
--- -------------------------------------------------------------------------- --
---                                  INVENTORY                                 --
--- -------------------------------------------------------------------------- --
+---@alias DeepIterateFilter function |"FilterByTag"|"FilterByTemplate"
+
+---@alias FilterMode string |"all"|"any"
 
 ---@alias DeepIterateOutput {[Guid] : DeepIterateElement}
 
@@ -14,86 +14,100 @@
 ---@field tags string[]
 ---@field amount integer
 
+-- -------------------------------------------------------------------------- --
+--                                  INVENTORY                                 --
+-- -------------------------------------------------------------------------- --
+
+---format our inv element
+---@param itemEntity ItemEntity
+---@return DeepIterateElement
+local function formatInventoryObjectData(itemEntity)
+    local uuid = itemEntity.Uuid and itemEntity.Uuid.EntityUuid
+    return {
+        name = GetTranslatedString(itemEntity.DisplayName.NameKey.Handle.Handle),
+        template = GUID(Osi.GetTemplate(uuid)) or "TemplateError",
+        tags = itemEntity.Tag.Tags or {},
+        statsId = itemEntity.Data.StatsId or "StatsIdError",
+        amount = Osi.GetStackAmount(uuid) or 0,
+        entity = itemEntity
+    }
+end
+
+---Filter by tag for deepiterate
+---@param filter table table of tags
+---@param filterMode? FilterMode filtering mode, any or all matches, defaults to all for tags
+function FilterByTag(filter, filterMode)
+    ---@param data DeepIterateElement
+    return function(data)
+        filterMode = filterMode or "all"
+
+        if filterMode == "all" then
+            if Table.CheckIfAllValuesExist(data.tags, filter) then
+                return data
+            else
+                return nil
+            end
+        elseif filterMode == "any" then
+            --write it
+        end
+    end
+end
+
+---Filter by template for deepiterate
+---@param filter table table of templates
+---@param filterMode? FilterMode filtering mode, any or all matches, defaults to any for templates
+function FilterByTemplate(filter, filterMode)
+    ---@param data DeepIterateElement
+    return function(data)
+        filterMode = filterMode or "any"
+
+        if filterMode == "all" then
+            --write it
+        elseif filterMode == "any" then
+            if Table.ContainsAny(filter, data.template) then
+                return data
+            else
+                return nil
+            end
+        end
+    end
+end
 
 --- Recursively iterates through an entity's inventory and all sub-inventories, building a table containing all items.
 ---@param entity any The entity whose inventory to iterate
 ---@param processedInventory? table Table to accumulate results
----@param tagFilter? table The table of tags to filter items (optional)
----@param templateFilter? table The table of templates to filter items (optional)
+---@param filterFuncs? DeepIterateFilter[] the filtering function
 ---@return DeepIterateOutput processedInventory Table containing all items in entity's inventory tree table format:
-function DeepIterateInventory(entity, tagFilter, templateFilter, processedInventory)
-    tagFilter = tagFilter or nil
-    templateFilter = templateFilter or nil
+function DeepIterateInventory(entity, filterFuncs, processedInventory)
     processedInventory = processedInventory or {}
     local entityInventory = entity.InventoryOwner.PrimaryInventory
     local entityItemsList = entityInventory.InventoryContainer.Items
-
     for _, entry in pairs(entityItemsList) do
         local item = entry.Item
         local isContainer = item.InventoryOwner ~= nil
         local StackMember = item.InventoryStackMember
-        local data = {
-            name = GetTranslatedString(item.DisplayName.NameKey.Handle.Handle),
-            template = GUID(Osi.GetTemplate(item.Uuid and item.Uuid.EntityUuid)) or "TemplateError",
-            tags = item.Tag.Tags or {},
-            statsId = item.Data.StatsId or "StatsIdError",
-            amount = Osi.GetStackAmount(item.Uuid and item.Uuid.EntityUuid) or 0,
-            entity = item
-        }
+        local data = formatInventoryObjectData(item)
         local uuid = item.Uuid and item.Uuid.EntityUuid or nil
-
-        -- Check if the item has all the specified tags
-        if tagFilter and not Table.CheckIfAllValuesExist(data.tags, tagFilter) then
-            -- Skip this item if it doesn't match all tags in the filter
-            goto continue
+        if filterFuncs then
+            for _, func in ipairs(filterFuncs) do
+                data = func(data)
+            end
         end
-
-        -- Check if the item's template matches at least one of the specified templates
-        if templateFilter and not Table.ContainsAny(templateFilter, data.template) then
-            -- Skip this item if it doesn't match any template in the filter
-            goto continue
-        end
-
-        if StackMember then
+        if StackMember and data then
             local itemStack = StackMember.Stack.InventoryStack.Arr_u64
             for _, stackElement in pairs(itemStack) do
-                local stackData = {
-                    name = GetTranslatedString(stackElement.DisplayName.NameKey.Handle.Handle),
-                    template = stackElement.ServerItem.Template.Id or "TemplateError",
-                    tags = stackElement.Tag.Tags or {},
-                    statsId = stackElement.Data.StatsId or "StatsIdError",
-                    amount = Osi.GetStackAmount(stackElement.Uuid.EntityUuid) or 0,
-                    entity = stackElement
-                }
+                local stackData = formatInventoryObjectData(stackElement)
                 local stackUuid = stackElement.Uuid and stackElement.Uuid.EntityUuid
-
-                -- Check if the item has all the specified tags
-                if tagFilter and not Table.CheckIfAllValuesExist(stackData.tags, tagFilter) then
-                    -- Skip this stack item if it doesn't match all tags in the filter
-                    goto continue_stack
-                end
-
-                -- Check if the item's template matches at least one of the specified templates
-                if templateFilter and not Table.ContainsAny(templateFilter, stackData.template) then
-                    -- Skip this stack item if it doesn't match any template in the filter
-                    goto continue_stack
-                end
-
                 processedInventory[stackUuid] = stackData
-
-                ::continue_stack::
             end
-        elseif not StackMember and uuid then
+        elseif not StackMember and uuid and data then
             processedInventory[uuid] = data
         end
 
-        ::continue::
-
         if isContainer then
-            DeepIterateInventory(item, tagFilter, templateFilter, processedInventory)
+            DeepIterateInventory(item, filterFuncs, processedInventory)
         end
     end
-
     return processedInventory
 end
 
@@ -109,14 +123,13 @@ end
 ---@param itemTemplate ROOT
 ---@return boolean result true if an item was given
 function GiveItemToEachPartyMember(itemTemplate)
-    local hasGivenPotion = false
+    local hasGivenItem = false
     GetSquadies()
     for _, player in pairs(SQUADIES) do
         if not HasItemTemplate(player, itemTemplate) then
             Osi.TemplateAddTo(itemTemplate, player, 1, 1)
-            hasGivenPotion = true
+            hasGivenItem = true
         end
     end
-    if hasGivenPotion then return true end
-    return false
+    return hasGivenItem
 end
