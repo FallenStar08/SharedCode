097c29a8a75fad16a93d75846d4f11769fd9fcc4
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-02-09 02:01:04 +0100

    DeepCopy deez nuts

--- Shared/Utils/Table.lua
+++ Shared/Utils/Table.lua
@@ -138,3 +138,18 @@ function Table.ContainsAny(target, value)
     end
     return false
 end
+
+function Table.DeepCopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[Table.DeepCopy(orig_key)] = Table.DeepCopy(orig_value)
+        end
+        setmetatable(copy, Table.DeepCopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
