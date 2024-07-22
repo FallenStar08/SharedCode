f5c515daf16362472b2bd8a70c113365b059852f
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-07-22 06:06:01 +0200

    Debounce UwU

--- Shared/Utils/Timer.lua
+++ Shared/Utils/Timer.lua
@@ -8,13 +8,14 @@ function MeasureExecutionTime(func)
     return elapsedTime
 end
 
-
-
 ---Delay a function call by the given time
 ---@param ms integer
 ---@param func function
 function DelayedCall(ms, func)
-    if ms == 0 then func() return end
+    if ms == 0 then
+        func()
+        return
+    end
     local Time = 0
     local handler
     handler = Ext.Events.Tick:Subscribe(function(e)
@@ -24,4 +25,22 @@ function DelayedCall(ms, func)
             Ext.Events.Tick:Unsubscribe(handler)
         end
     end)
+end
+
+---Bouncy bounce
+---@param fn function the callback
+---@param delay number delay in sec
+---@return function
+function Debounce(fn, delay)
+    local timer = nil
+    return function(...)
+        local args = { ... }
+        if timer then
+            Ext.Timer.Cancel(timer)
+        end
+        timer = Ext.Timer.WaitFor(delay * 1000, function()
+            fn(table.unpack(args))
+            timer = nil
+        end)
+    end
 end
